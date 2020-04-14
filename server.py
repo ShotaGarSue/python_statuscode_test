4b35d0d93e71e22eaa00a970ff28cbb39bb74db4
Shota Suga <6shota.suga6@gmail.com>
2020-04-14 13:04:52 +0900

    add post method endpoints

--- server.py
+++ server.py
@@ -5,12 +5,23 @@ app = Flask(__name__)
 @app.route("/200")
 def route200():
     return "Result 200"
+@app.route("/200", methods=["POST"])
+def route200post():
+    return "POST Result 200"
+
 @app.route("/400")
 def route400():
     return "Result 400", 400 
+@app.route("/400", methods=["POST"])
+def route400post():
+    return "POST Result 400", 400 
+
 @app.route("/500")
 def route500():
     return "Result 500", 500
+@app.route("/500", methods=["POST"])
+def route500post():
+    return "POST Result 500", 500
 
 if __name__ == "__main__":
     app.run()
